68a3ca7e59c77103e992fd24262323d679fa455b
EC2 Default User <ec2-user@ip-172-31-95-24.ec2.internal>
2021-11-06 13:06:49 +0000

    [Update] sign up／log inページの編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
+  get 'home/about' => 'homes#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :user, only: [:index, :show, :edit, :update]
-  resources :book, only: [:index, :show, :edit, :create, :update, :destroy]
+  resources :users, only: [:index, :show, :edit, :update]
+  resources :books, only: [:index, :show, :edit, :create, :update, :destroy]
 
 end
